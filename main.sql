54e18c5249766f9e111cd3596d2de56b1dec6a09
AxelCarrillo <122809603+AxelCarrillo@users.noreply.github.com>
2023-05-17 17:22:08 +0000

    ACTUALIZATED importar el csv

--- main.sql
+++ main.sql
@@ -1,13 +1,15 @@
+.headers on
+.mode box
+.import personas.csv personas
 
-CREATE TABLE Personas (
-    id_persona INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre VARCHAR(40) NOT NULL,
-    primer_apellido VARCHAR(20) NOT NULL,
-    segundo_apellido VARCHAR(20) NOT NULL,
-    email VARCHAR(30) NOT NULL );
-INSERT INTO Personas (nombre, primer_apellido, segundo_apellido, email)
-VALUES
-    ('Axel', 'Carrillo', 'Rocha', 'axel.carrillo@example.com'),
-    ('Carol', 'Jimenez', 'Martinez', 'carol.jimenez@example.com'),
-    ('Kayla', 'Carrillo', 'Jimenez', 'kaylacarji@example.com');
-SELECT * FROM Personas;
+SELECT CREAR TABLA personas;
+
+CREATE TABLE personas(
+    id_persona    INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre        VARCHAR(40)    NOT NULL,
+    primer_apellido    VARCHAR(20)    NOT NULL,
+    segundo_apellido    VARCHAR(20)    NOT NULL,
+    email    VARCHAR(30)    NOT NULL
+);
+
+SELECT * FROM personas;
